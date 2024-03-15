9589aff5dd3de3e67e03db45782966ea1056f195
green-apex-bhagyashri <bhagyashri@green-apex.com>
2024-03-15 15:17:27 +0530

    change suser scontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
-  before_action :authorize_request, except: :create
+  before_action :authorize_request, except: [:create, :index, :show]
   before_action :find_user, except: %i[create index]
 
   # GET /users
   def index
     @users = User.all
-    render json: UserSerializer.new(@user).serializable_hash, status: :ok
+    # render json: ActiveModelSerializers::Adapter::Json.new(UserSerializer.new(@users)), status: :ok
+    render json: @users, each_serializer: UserSerializer, status: :ok
   end
 
   # GET /users/{username}
   def show
-    render json: @user, status: :ok
+    # render json: ActiveModelSerializers::Adapter::Json.new(UserSerializer.new(@users)).serializable_hash, status: :ok
+    render json: @user, serializer: UserSerializer, status: :ok
   end
 
   # POST /users
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       render json: @user, status: :created
     else
-      render json: { errors: @user.errors.full_messages },
+      render json: { errors: @user.errors},
              status: :unprocessable_entity
     end
   end
